b5816cc26deddf210720277726b7fca863efe311
guinpen98 <yuri.pocha.0908@gmail.com>
2021-12-23 02:12:05 +0900

    動く円を一定の確率で生成

--- savannah/main.cpp
+++ savannah/main.cpp
@@ -1,40 +1,51 @@
 #include"init.hpp"
 #include<random>
+#include<array>
+#include<vector>
 
 struct vec2
 {
     int x=0, y=0;
 };
+
 class Draw {
+public:
+    void circleDraw(const int x, const int y) {
+        DrawCircle(x, y, 10, GetColor(255, 255, 255), TRUE);
+    }
+};
+class Actor {
 private:
     int x = 256;
-    int x2 = 150;
     int y = 150;
-    int y2 = 256;
 public:
-    void circleDraw(const int moveX,const int moveY, const int moveX2, const int moveY2) {
+    void move(const int moveX, const int moveY) {
+        x += moveX, y += moveY;
         if (x < 0) x = 0;
         else if (x > windowWidth) x = windowWidth;
         if (y < 0) y = 0;
         else if (y > windowHeight) y = windowHeight;
-        if (x2 < 0) x2 = 0;
-        else if (x2 > windowWidth) x2 = windowWidth;
-        if (y2 < 0) y2 = 0;
-        else if (y2 > windowHeight) y2 = windowHeight;
-        DrawCircle(x+moveX, y+moveY, 10, GetColor(0, 0, 255), TRUE);
-        DrawCircle(x2+moveX2,y2+moveY2, 10, GetColor(255, 0, 0), TRUE);
+        Draw draw;
+        draw.circleDraw(x, y);
     }
 };
 
+
 void Main() {
-    Draw draw;
+    //Draw draw[3];
+    //std::array<Actor,3> actor;
+    std::vector<Actor> actor;
+
     constexpr int MIN = -20;
     constexpr int MAX = 20;
     std::random_device rd;
     std::mt19937 eng(rd());
     std::uniform_int_distribution<int> distr(MIN, MAX);
+    //確率
+    std::bernoulli_distribution uid(0.01);
     while (System::Update()) {
-        draw.circleDraw(distr(eng), distr(eng), distr(eng), distr(eng));
+        if (uid(eng)) actor.emplace_back();
+        for(auto& a:actor) a.move(distr(eng), distr(eng));
         
 
     }
