c4f0a1e0700182a5af65cc5a76b94fcb76e24442
guinpen98 <yuri.pocha.0908@gmail.com>
2021-12-24 17:11:12 +0900

    AnimalクラスとHerbivoreクラスの追加 ランダムに目的地を決めて移動する機能の追加

--- savannah/main.cpp
+++ savannah/main.cpp
@@ -2,6 +2,8 @@
 #include<random>
 #include<array>
 #include<vector>
+#include <chrono>
+#include <cmath>
 
 struct vec2
 {
@@ -10,44 +12,82 @@ struct vec2
 
 class Draw {
 public:
-    void circleDraw(const int x, const int y) {
-        DrawCircle(x, y, 10, GetColor(255, 255, 255), TRUE);
+    void circleDraw(const int x, const int y,const int number) {
+        DrawCircle(x, y, 5, GetColor(255, 255, 255), TRUE);
     }
 };
-class Actor {
+class Animal {
 private:
-    int x = 256;
-    int y = 150;
+    double x = 256;
+    double y = 256;
+    double distinationX = 256;
+    double distinationY = 256;
+protected:
+    double update = 0;
 public:
-    void move(const int moveX, const int moveY) {
-        x += moveX, y += moveY;
-        if (x < 0) x = 0;
-        else if (x > windowWidth) x = windowWidth;
-        if (y < 0) y = 0;
-        else if (y > windowHeight) y = windowHeight;
+    void move(const int number) {
+        double distance = calculateDistance(x, y, distinationX, distinationY);
+        double dx = (distinationX-x) / distance;
+        double dy = (distinationY-y) / distance;
+        x += dx , y += dy;
         Draw draw;
-        draw.circleDraw(x, y);
+        draw.circleDraw(x, y,number);
+    } 
+    bool isDistination() {
+        if ((x -distinationX)<1 && (y-distinationY)<1) return true;
+        else return false;
+    }
+    double calculateDistance(const int x,const int y,const int dsX,const int dsY) {
+        return sqrt(pow(x - dsX, 2) + pow(y - dsY, 2));
+    }
+
+    void setDistination() {
+        constexpr int MIN = -100;
+        constexpr int MAX = 100;
+        std::random_device rd;
+        std::mt19937 eng(rd());
+        std::uniform_int_distribution<int> distr(MIN, MAX);
+        do {
+            distinationX = x + distr(eng);
+        }while (distinationX<0 || distinationX>windowWidth);
+        do {
+            distinationY = y + distr(eng);
+        } while (distinationY<0 || distinationY>windowHeight);
+    }
+
+    void Update(const double up) {
+        update = up;
+    }
+};
+class Herbivore : public Animal {
+public:
+    void behavior(){
+        if (isDistination()) setDistination();
+        move(0);
     }
 };
 
 
 void Main() {
-    //Draw draw[3];
-    //std::array<Actor,3> actor;
-    std::vector<Actor> actor;
+    //Draw draw[3]
+    std::vector<Herbivore> herbivore;
 
-    constexpr int MIN = -20;
-    constexpr int MAX = 20;
+    std::chrono::system_clock::time_point  start, end;
+
     std::random_device rd;
     std::mt19937 eng(rd());
-    std::uniform_int_distribution<int> distr(MIN, MAX);
     //確率
     std::bernoulli_distribution uid(0.01);
     while (System::Update()) {
-        if (uid(eng)) actor.emplace_back();
-        for(auto& a:actor) a.move(distr(eng), distr(eng));
-        
+        end = std::chrono::system_clock::now();
 
+        if (uid(eng)) herbivore.emplace_back();
+        for (auto& h : herbivore) {
+            h.Update(std::chrono::duration_cast<std::chrono::seconds>(end - start).count());
+            h.behavior();
+        }
+
+        start = std::chrono::system_clock::now();
     }
 
 }
